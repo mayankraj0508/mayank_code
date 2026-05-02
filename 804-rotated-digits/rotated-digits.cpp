a22febe58a44af976bde5280305ff9928f17f585
mayankraj0508 <mrajdbg2018@gmail.com>
2026-05-02 17:57:09 +0530

    Time: 1 ms (44.56%) | Memory: 7.9 MB (46.67%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,64 +1,32 @@
 class Solution {
 public:
     bool check(int x){
-        int prev = x;
-        if(x==0){
-            return false;
-        }
-        int  news = 0;
-        int product = 1;
-        while(x>0){
-            int rem = x%10;
-            if(rem==3 ||rem==4 || rem==7){
+        bool changed = false;
+
+        while(x > 0){
+            int rem = x % 10;
+
+            if(rem == 3 || rem == 4 || rem == 7){
                 return false;
             }
-            else if(rem==0 || rem==1 ||rem==8){
-              news = rem*product +news;
-              product = product*10;
-            }
-            else if(rem==2 || rem==5){
-                if(rem==2){
-                    rem = 5;
-                    news = rem*product +news;
-                     product = product*10;
-                }
-                else{
-                    rem=2;
-                    news = rem*product +news;
-                     product = product*10;
-                }
-            }
-            else{
-                   if(rem==6){
-                    rem = 9;
-                    news = rem*product +news;
-                     product = product*10;
-                }
-                else{
-                    rem=6;
-                    news = rem*product +news;
-                     product = product*10;
-                }
-                
-            }
-            x = x/10;
 
+            if(rem == 2 || rem == 5 || rem == 6 || rem == 9){
+                changed = true;
+            }
+
+            x /= 10;
         }
-       if(prev==news){
-        return false;
-       }
-       
-       return true;
+
+        return changed;
     }
+
     int rotatedDigits(int n) {
         int count = 0;
-        for(int i = 1; i<=n; i++){
+        for(int i = 1; i <= n; i++){
             if(check(i)){
                 count++;
             }
-
         }
         return count;
-        
     }
 };
