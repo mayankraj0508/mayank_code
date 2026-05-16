f22525e2a9e47a32f349a762846dd00cb7142352
mayankraj0508 <mrajdbg2018@gmail.com>
2026-05-16 10:32:13 +0530

    Time: 15 ms (38.15%) | Memory: 12.3 MB (41.86%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
-        unordered_map<char,int> m;
-
-        int i = 0;
-        int j = 0;
-
-        int ans = 0;
-
-        while(j < s.size()) {
-
+        unordered_map<char,int>m;
+        int i =0;
+        int j =0;
+        int ans =  1;
+        if(s.size()==1){
+            return 1;
+        }
+        if(s.size()==0){
+            return 0;
+        }
+        bool flag = false;
+        while( j<s.size()){
             m[s[j]]++;
-
-            // duplicate mila
-            if(m[s[j]] > 1) {
-
-                // duplicate aane se pehle wali valid length
-                ans = max(ans, j - i);
-
-                // duplicate hatao
-                while(s[i] != s[j]) {
-
+            if(m[s[j]]>=2){
+                flag = true;
+                 ans = max(ans,j-i);
+                while( i<s.size() && s[i]!=s[j]){
+                    
                     m[s[i]]--;
-
-                    if(m[s[i]] == 0) {
+                    if(m[s[i]]==0){
                         m.erase(s[i]);
                     }
-
                     i++;
                 }
-
-                // old duplicate remove
-                m[s[i]]--;
+                
+                m[s[j]]--;
                 i++;
+          
             }
-
             j++;
+          
+            
         }
-
-        // ho sakta h last tak kabhi duplicate aaya hi na ho
-        ans = max(ans, j - i);
-
-        return ans;
+        ans = max(ans,j-i);
+        return  ans;
+        
     }
 };
